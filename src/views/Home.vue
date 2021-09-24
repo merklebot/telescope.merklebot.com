1f8ffc7f53a34b1a116c6927f2fcf1ad486cb813
Vitaly <vitl2907@me.com>
2021-09-24 11:29:24 -0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,14 @@
     <h1>Spot SDK Eucational Program</h1>
 
     <div>
-      We are excited to see you join the Spot SDK educational program.<br />Our
-      goal is to get you familiar with Spot's SDK and allow you to test your
+      <br />We are excited to see you join the Spot SDK educational program. Welcome!<br />
+      <br />Our goal is to get you familiar with Spot's SDK and allow you to test your
       skills using a Boston Dynamics Spot robot in our lab.<br />
-      >
+      <br />We recommend you to study the theoretical materials before you rent the time on Spot so that you have a plan of action 
+      for your lesson and can complete it successfully.<br />
+      <br />Also, you will have to install additional software to you machine in order to get access to Spot remotly. You can find the instructions in Lesson 0 of the Thery section.<br />
+      <br />Welcome to our robotics lab and we hope that you will enjoy your time with Spot!<br />
+      
     </div>
 
     <div style="margin:30px 0">
