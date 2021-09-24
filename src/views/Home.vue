20970177c5760aff1ef0887b0c79f36f80d0e312
Vitaly <vitl2907@me.com>
2021-09-24 10:50:01 -0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,7 @@
     <div>
       We are excited to see you join the Spot SDK educational program.<br />Our
       goal is to get you familiar with Spot's SDK and allow you to test your
-      skills using a Boston Dynamics Spot robot in our lab.<br /><br />Welcome
-      to the first lesson!<br />During this lesson you will learn how to
-      authorize yourself as a user, get motor power control and send basic
-      commands to Spot.<br /><br /><em
-        >"This is a demo, not the real lesson yet"</em
+      skills using a Boston Dynamics Spot robot in our lab.<br />
       >
     </div>
 
