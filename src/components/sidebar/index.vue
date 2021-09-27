6f903da96e7ede90a542c0c458eadc3f415a2daa
Alisher A. Khassanov <a.khssnv@gmail.com>
2021-09-27 15:46:29 +0300

    Remove menu item with Boston Dynamics docs

--- src/components/sidebar/index.vue
+++ src/components/sidebar/index.vue
@@ -12,13 +12,6 @@
       <router-link :to="{ name: 'schedule' }" active-class="active" exact>
         Schedule your time
       </router-link>
-
-      <a
-        href="https://github.com/boston-dynamics/spot-sdk/blob/master/docs/python/understanding_spot_programming.md"
-        target="_blank"
-      >
-        Study the documentation
-      </a>
     </Section>
     <Section v-if="lessons.length" title="Theory">
       <router-link
