959d3dc16bf0f8d4fe66e2344bc684f41432b6bf
Alisher A. Khassanov <a.khssnv@gmail.com>
2021-12-06 14:02:12 +0300

    Specify public path on gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: "",
+  publicPath: "/beyond-the-sky/",
   configureWebpack: {
     resolve: {
       extensions: ["*", ".mjs", ".js", ".vue", ".json", ".gql", ".graphql"]
