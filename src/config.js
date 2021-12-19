86947bb0ff438dea83dd092c3a46b8ae4504d223
Alisher A. Khassanov <a.khssnv@gmail.com>
2021-12-19 21:19:43 +0400

    Configure access token ID and receiver from env

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export default {
     process.env.VUE_APP_API_SERVER ?? "https://checkout.spot-sdk.education",
   STRIPE_KEY: process.env.VUE_APP_STRIPE_KEY ?? "pk_................",
   PRICE_PER_LESSON_CENTS: process.env.VUE_APP_PRICE_PER_LESSON_CENTS ?? 2000,
-  ID_ASSET: 1441,
+  ID_ASSET: process.env.VUE_APP_ACCESS_TOKEN_ASSET_ID,
   SUBSTRATE: {
     url: "wss://statemine-rpc.polkadot.io",
     types: {
@@ -20,5 +20,6 @@ export default {
       isDevelopment: false,
       type: "sr25519"
     }
-  }
+  },
+  ACCESS_TOKEN_RECV_ACCOUNT: process.env.VUE_APP_ACCESS_TOKEN_RECV_ACCOUNT
 };
