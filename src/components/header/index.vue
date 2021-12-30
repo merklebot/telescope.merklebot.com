ee847f127d8bbb5d416caf0db9b30b1584938165
Vitaly <vitl2907@me.com>
2021-12-29 17:54:21 -0800

    Texts adjustment

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -20,7 +20,7 @@
           <h4>First Beyond the Sky</h4>
           <p>This digital experience explores the interconnection of realverse and metaverse. </p>
           <p><a href="https://medium.com/merklebot/mint-nfts-with-telescope-in-chile-371eb773189">Learn more</a></p>
-          <h4>Uses technologies</h4>
+          <h4>Technologies used</h4>
           <p>Blockchain, <a href="https://robonomics.network/">Robonomics</a>, <a href="https://kusama.network/">Kusama</a>, <a href="https://ipfs.io/">IPFS</a>, <a href="https://www.rmrk.app/">RMRK</a></p>
         </div>
       </details>
