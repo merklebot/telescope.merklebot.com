084e204384c599281b4571c9ec68fd8993356059
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-02-08 22:21:22 +0400

    Remove views not used

--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Success from "@/views/Success";
-import Cancel from "@/views/Cancel";
 import Checkout from "@/views/Checkout";
 
 Vue.use(Router);
@@ -12,16 +10,6 @@ export default new Router({
       path: "/",
       name: "checkout",
       component: Checkout
-    },
-    {
-      path: "/success",
-      name: "success",
-      component: Success
-    },
-    {
-      path: "/cancel",
-      name: "cancel",
-      component: Cancel
     },
     { path: "*", redirect: "/" }
   ]
--- src/views/Cancel.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <Card style="text-align:center">
-    <h1 class="red">Transaction was cancelled</h1>
-    <p>Your payment was cancelled, would you like to <router-link :to="{ name: 'checkout' }">begin the process again</router-link>?</p>
-  </Card>
-</template>
