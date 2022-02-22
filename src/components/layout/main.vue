1e6358ec11d13734ff5a6ce76bc96381d72c3b55
A. Bakai @positivecrash <nastin_box@list.ru>
2022-02-22 15:00:08 +0400

    Discord help link

--- src/components/layout/main.vue
+++ src/components/layout/main.vue
@@ -6,6 +6,18 @@
       <slot />
     </main>
 
+    <a 
+    title="Get support in our Discord community" 
+    class="support-discord" 
+    :href="$discord" 
+    target="_blank">
+    <img alt="" src="i/discord.png" aria-hidden="true"/>
+    <div class="support-discord-content">
+      <div class="support-discord-title">Any troubles?</div>
+      <div>Ask your question in Discord!</div>
+    </div>
+    </a>
+
   </fragment>
 </template>
 
@@ -19,3 +31,35 @@ export default {
   }
 };
 </script>
+
+<style>
+.support-discord {
+  position: fixed;
+  z-index: 1000;
+  bottom: var(--space);
+  right: var(--space);
+
+  display: grid;
+  grid-template-columns: 60px 1fr;
+  gap: var(--space);
+
+  background-color: var(--color-blue);
+  padding: var(--space);
+  border-radius: var(--space);
+  border: 2px solid var(--color-lilac);
+  max-width: calc(100% - var(--space) * 2);
+}
+
+.support-discord img {
+  max-width: 60px;
+}
+
+.support-discord-content {
+  font-size: 80%;
+}
+
+.support-discord-title {
+  font-weight: bold;
+  font-size: var(--font-size);
+}
+</style>
